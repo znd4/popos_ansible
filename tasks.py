f3cc2ce2d3c14a8a7911d687fbf127a2a45ecb16
Zane Dufour <zane.dufour@gmail.com>
2020-10-04 12:07:45 -0400

    add more stuff

--- tasks.py
+++ tasks.py
@@ -13,7 +13,7 @@ def pull(c, force=False):
     """run ansible pull to sync"""
     c.sudo("sudo apt-get update")
     c.sudo("sudo apt-get install -y ansible")
-    packages = ["oefenweb.slack"]
+    packages = ["oefenweb.slack", "stephdewit.nvm"]
     for package in packages:
         c.sudo(f"/usr/bin/ansible-galaxy install {package}")
     collections = ["community.general"]
