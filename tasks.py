b924131f1ab4f6e3c395d7ef7944b9d08dfbd9b6
Zane Dufour <zane.dufour@gmail.com>
2020-10-07 09:05:44 -0400

    add run

--- tasks.py
+++ tasks.py
@@ -7,10 +7,8 @@ def get_sudo_password(c):
     """Prompt user for their sudo password, then try to set the sudo password"""
     c.config.sudo.password = getpass.getpass("sudo password: ")
 
-
 @task(get_sudo_password)
-def pull(c, force=False):
-    """run ansible pull to sync"""
+def dependencies(c, force=False):
     c.sudo("sudo apt-get update")
     c.sudo("sudo apt-get install -y ansible")
     packages = ["oefenweb.slack", "stephdewit.nvm"]
@@ -19,6 +17,16 @@ def pull(c, force=False):
     collections = ["community.general"]
     for collection in collections:
         c.sudo(f"/usr/bin/ansible-galaxy collection install {collection}")
+
+@task(dependencies)
+def run(c, force=False):
+    c.sudo(
+        "/usr/bin/ansible-playbook local.yml"
+    )
+
+@task(dependencies)
+def pull(c, force=False):
+    """run ansible pull to sync"""
     c.sudo(
         "/usr/bin/ansible-pull "
         f"{'--force' if force else ''} "
