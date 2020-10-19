ec9f7207c64efe05cf8ca441a538d72f2b123731
Zane Dufour <zanedufour@pop-os.localdomain>
2020-10-19 07:33:26 -0400

    try adding personal_users

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 from invoke import task
 import getpass
+import json
+import sys
 
 
 @task
@@ -7,8 +9,17 @@ def get_sudo_password(c):
     """Prompt user for their sudo password, then try to set the sudo password"""
     c.config.sudo.password = getpass.getpass("sudo password: ")
 
+
 @task(get_sudo_password)
 def dependencies(c, force=False):
+    c.run(f"{sys.executable} -m python pip install inquirer")
+    import inquirer
+
+    c.config.personal_users = inquirer.checkbox(
+        message="Which users should the setup process be run for?",
+        choices=["zdufour", "zanedufour"],
+    )
+
     c.sudo("sudo apt-get update")
     c.sudo("sudo apt-get install -y ansible")
     packages = ["oefenweb.slack", "stephdewit.nvm"]
@@ -18,11 +29,15 @@ def dependencies(c, force=False):
     for collection in collections:
         c.sudo(f"/usr/bin/ansible-galaxy collection install {collection}")
 
+
 @task(dependencies)
 def run(c, force=False):
     c.sudo(
-        "/usr/bin/ansible-playbook local.yml"
+        f"/usr/bin/ansible-playbook "
+        f"--extra-vars='{json.dumps(dict(personal_users=c.config.personal_users))}' "
+        "local.yml"
     )
+
 
 @task(dependencies)
 def pull(c, force=False):
@@ -31,6 +46,7 @@ def pull(c, force=False):
         "/usr/bin/ansible-pull "
         f"{'--force' if force else ''} "
         "--accept-host-key "
+        f"--extra-vars='{json.dumps(dict(personal_users=c.config.personal_users))}'"
         "--private-key=/home/zanedufour/.ssh/id_rsa "
         "-U "
         "git@github.com:zdog234/popos_ansible.git"
